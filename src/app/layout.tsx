0f3c704199697ed7207ed0e78b5f5810131c4b76
jong-k <fully.charged07@gmail.com>
2025-09-04 23:52:45 +0900

    feat: pretendard font 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,18 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "BlinkDM",
   description: "실시간 채팅 앱",
 };
+
+const pretendard = localFont({
+  src: "../../public/fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-pretendard",
+});
 
 export default function RootLayout({
   children,
@@ -13,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className="antialiased">{children}</body>
+      <body className={`${pretendard.className} antialiased`}>{children}</body>
     </html>
   );
 }
